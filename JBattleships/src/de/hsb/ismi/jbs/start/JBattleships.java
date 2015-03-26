4437796d4ebcc28970cc9f737676675c9aff7ee4
Kevin Kuegler <kekuegler@stud.hs-bremen.de>
2015-03-26 02:53:06 +0100

    Initial Commit

---
+++ JBattleships/src/de/hsb/ismi/jbs/start/JBattleships.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package de.hsb.ismi.jbs.start;
+
+import java.awt.EventQueue;
+
+import javax.swing.UIManager;
+
+/**
+ * @author Kevin Kuegler
+ * @version 1.00
+ */
+public class JBattleships {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel"); //ALWAYS SET BEFORE CREATING THE FRAME!
+					//new UOIGUI();
+					System.out.println("TEST OUTPUT");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+}
+
