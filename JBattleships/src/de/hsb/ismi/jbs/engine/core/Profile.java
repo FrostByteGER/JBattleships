fdbdfc4eb2328d563fe23783174ba0bac3d714ae
Kevin Kuegler <kekuegler@stud.hs-bremen.de>
2015-04-23 21:11:03 +0200

    Added Writer classes and core stuff

---
+++ JBattleships/src/de/hsb/ismi/jbs/engine/core/Profile.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package de.hsb.ismi.jbs.engine.core;
+
+/**
+ * @author Kevin Kuegler
+ * @version 1.00
+ */
+public class Profile {
+
+	/**
+	 * 
+	 */
+	public Profile() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
