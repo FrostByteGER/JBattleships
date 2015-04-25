c62633618bc3c7aa1929356e96b6e7638dbffdb4
Kevin Kuegler <kekuegler@stud.hs-bremen.de>
2015-04-25 03:35:55 +0200

    Added Settingscontent, GameType enum and restore function.

--- JBattleships/src/de/hsb/ismi/jbs/gui/JBSGUI.java
+++ JBattleships/src/de/hsb/ismi/jbs/gui/JBSGUI.java
@@ -1,6 +1,7 @@
 package de.hsb.ismi.jbs.gui;
 
 import java.awt.BorderLayout;
+import java.util.Stack;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -17,11 +18,13 @@ public class JBSGUI{
 
 	private JFrame mainFrame;
 	private JPanel contentPane;
+	private Stack<JPanel> panelStack;
 
 	/**
 	 * Create the frame and its components.
 	 */
 	public JBSGUI() {
+		panelStack = new Stack<JPanel>();
 		mainFrame = new JFrame("JBattleships ALPHA");
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainFrame.setBounds(100, 100, JBSCore.resolutions[0][0], JBSCore.resolutions[0][1]);
@@ -29,7 +32,7 @@ public class JBSGUI{
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		//TODO: Remove add call
-		contentPane.add(new JBSOptionsPanel(),BorderLayout.CENTER);
+		contentPane.add(new JBSOptionsPanel(this),BorderLayout.CENTER);
 		mainFrame.setContentPane(contentPane);
 		mainFrame.setLocationRelativeTo(null); // Sets GUI to center of the screen
 		mainFrame.setVisible(true); // Call always at the end!
@@ -44,13 +47,27 @@ public class JBSGUI{
 	}
 	
 	/**
-	 * Swaps the current center-component with the given one
+	 * Swaps the current center-component with the given one.
 	 * @param container
 	 */
 	public void swapContainer(JPanel container){
 		//TODO: Check if remove works!
+		panelStack.push((JPanel) ((BorderLayout)contentPane.getLayout()).getLayoutComponent(BorderLayout.CENTER));
 		contentPane.remove(((BorderLayout)contentPane.getLayout()).getLayoutComponent(BorderLayout.CENTER));
 		contentPane.add(container,BorderLayout.CENTER);
+		JBSCore.msgLogger.addMessage("Swapped JPanel!");
+	}
+	
+	/**
+	 * Restores the previous JPanel that was visible before a new one was called.
+	 */
+	public void restorePrevContainer(){
+		if(!panelStack.empty()){
+			swapContainer(panelStack.pop());
+			JBSCore.msgLogger.addMessage("Restored previous JPanel!");
+		}else{
+			JBSCore.msgLogger.addMessage("Stack is Empty!");
+		}
 	}
 
 }
