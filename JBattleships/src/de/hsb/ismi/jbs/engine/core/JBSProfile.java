df7ba0edad58a5c28f6a4dcd35d1afe6cf014ef7
Yuari <reptile4077@hotmail.de>
2015-04-30 15:18:18 +0200

    profile manager and parser

--- JBattleships/src/de/hsb/ismi/jbs/engine/core/JBSProfile.java
+++ JBattleships/src/de/hsb/ismi/jbs/engine/core/JBSProfile.java
@@ -3,12 +3,22 @@
  */
 package de.hsb.ismi.jbs.engine.core;
 
+import java.util.HashMap;
+
 /**
  * @author Kevin Kuegler
  * @version 1.00
  */
 public class JBSProfile {
-
+	
+	private int id;
+	private String name;
+	private HashMap<String,Float> stats;
+	private HashMap<String,Achievement> achievements;
+	
+	
+	
+	
 	/**
 	 * 
 	 */
