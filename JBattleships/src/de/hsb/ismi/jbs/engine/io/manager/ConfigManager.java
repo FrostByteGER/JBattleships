176a45c56c2f17f9d5f165f7cff65b18669c030a
Kevin Kuegler <kekuegler@stud.hs-bremen.de>
2015-05-06 16:39:18 +0200

    Finalized most Managers and Parsers

--- JBattleships/src/de/hsb/ismi/jbs/engine/io/manager/ConfigManager.java
+++ JBattleships/src/de/hsb/ismi/jbs/engine/io/manager/ConfigManager.java
@@ -27,7 +27,7 @@ public class ConfigManager {
 	 */
 	public ConfigManager() {
 		this.parser = new ConfigParser();
-		data = new HashMap<>();
+		data = new HashMap<>(0);
 	}
 	
 	/**
@@ -35,17 +35,54 @@ public class ConfigManager {
 	 * @return The parsed and sorted data. Null if error encountered.
 	 */
 	public HashMap<String, HashMap<String, String>> loadConfig(){
+		HashMap<String, HashMap<String, String>> data = new HashMap<>(0);
 		try {
-			ArrayList<String> parsedData = parser.parseConfig(JBSCore.DATA_PATH + CONFIG_PATH + CONFIG_NAME);
-			
-			for(String s : parsedData){
-				//TODO: Add Code
+			ArrayList<String> raw = parser.parseConfig(JBSCore.DATA_PATH + CONFIG_PATH + CONFIG_NAME);
+			ArrayList<String> tempArray = null;
+			String category = null;
+			for(String s : raw){
+				if(s.startsWith(ConfigParser.CATEGORY_DELIMITERS[0]) && s.endsWith(ConfigParser.CATEGORY_DELIMITERS[1])){
+					if(category != null && !category.equals(s.substring(1, s.length()-1))){
+						HashMap<String, String> temp = new HashMap<String, String>(0);
+						for(int i = 0;i<tempArray.size();i+=2){
+							temp.put(tempArray.get(i), tempArray.get(i+1));
+						}
+						data.put(category, temp);
+					}
+					category = s.substring(1, s.length()-1);
+					tempArray = new ArrayList<>(0);
+				}else{
+					tempArray.add(s);
+					if(raw.get(raw.size() - 1).equals(s)){
+						HashMap<String, String> temp = new HashMap<String, String>(0);
+						for(int i = 0;i<tempArray.size();i+=2){
+							temp.put(tempArray.get(i), tempArray.get(i+1));
+						}
+						data.put(category, temp);
+					}
+				}
 			}
 		} catch (IOException | JBSParserException e) {
 			e.printStackTrace();
+			//JBSCore.msgLogger.addException(e);
 			return null;
 		}
-		return null;
+		this.data = data;
+		return data;
+	}
+
+	/**
+	 * @return the data
+	 */
+	public final HashMap<String, HashMap<String, String>> getData() {
+		return data;
+	}
+
+	/**
+	 * @return the parser
+	 */
+	public final ConfigParser getParser() {
+		return parser;
 	}
 
 }
--- JBattleships/src/de/hsb/ismi/jbs/engine/io/manager/OptionsManager.java
+++ JBattleships/src/de/hsb/ismi/jbs/engine/io/manager/OptionsManager.java
@@ -20,8 +20,8 @@ import de.hsb.ismi.jbs.engine.io.parser.OptionsParser;
 public class OptionsManager {
 	
 	private OptionsParser parser;
-	private final String[] CATEGORIES = {"Graphics","Audio","Network"};
-	private HashMap<String, String[]> data;
+	private static final String[] CATEGORIES = {"Graphics","Audio","Network"};
+	private HashMap<String, HashMap<String, String>> data;
 	private static final String SETTINGS_PATH = "Config/";
 	private static final String SETTINGS_NAME = "Settings.cfg";
 
@@ -37,25 +37,33 @@ public class OptionsManager {
 	 * 
 	 * @return The parsed and sorted data. Null if error encountered.
 	 */
-	public HashMap<String, String[]> loadOptions(){
+	public HashMap<String, HashMap<String, String>> loadOptions(){
 		ArrayList<String> raw = new ArrayList<>(0);
 		ArrayList<String> currentData = new ArrayList<>(0);
-		HashMap<String, String[]> data = new HashMap<>();
+		HashMap<String, HashMap<String, String>> data = new HashMap<>(0);
 		String currentCategory = "";
 		try {
 			raw = parser.parseOptions(JBSCore.DATA_PATH + SETTINGS_PATH + SETTINGS_NAME);
 			for(String s : raw){
 				if(s.equals(CATEGORIES[0]) || s.equals(CATEGORIES[1]) || s.equals(CATEGORIES[2])){
 					if(!currentCategory.equals(s) && !currentCategory.isEmpty()){
-						data.put(currentCategory, currentData.toArray(new String[currentData.size()]));
+						HashMap<String, String> temp = new HashMap<String, String>(0);
+						for(int i = 0;i<currentData.size();i+=2){
+							temp.put(currentData.get(i), currentData.get(i+1));
+						}
+						data.put(currentCategory, temp);
 						currentData = new ArrayList<>(0);
 					}
 					currentCategory = s;
-				}else if(raw.get(raw.size() - 1).equals(s)){
-					currentData.add(s);
-					data.put(currentCategory, currentData.toArray(new String[currentData.size()]));
 				}else{
 					currentData.add(s);
+					if(raw.get(raw.size() - 1).equals(s)){
+						HashMap<String, String> temp = new HashMap<String, String>(0);
+						for(int i = 0;i<currentData.size();i+=2){
+							temp.put(currentData.get(i), currentData.get(i+1));
+						}
+						data.put(currentCategory, temp);
+					}
 				}
 			}
 		} catch (FileNotFoundException fnfe) {
