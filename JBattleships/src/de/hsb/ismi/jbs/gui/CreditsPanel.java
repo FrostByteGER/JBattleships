d40f923097d17c61097be213d6c388506641772f
Kevin Kuegler <kekuegler@stud.hs-bremen.de>
2015-07-06 22:48:31 +0200

    Fixed Naming

--- JBattleships/src/de/hsb/ismi/jbs/gui/CreditsPanel.java
+++ JBattleships/src/de/hsb/ismi/jbs/gui/CreditsPanel.java
@@ -7,13 +7,17 @@ import javax.swing.JPanel;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.BorderLayout;
+import javax.swing.JLabel;
 
 /**
  * @author Kevin Kuegler
  * @version 1.00
  */
 public class CreditsPanel extends JPanel {
-	private JPanel panel;
+	private JPanel centerPanel;
+	private JLabel lblNewLabel;
+	private JLabel lblNewLabel_1;
+	private JLabel lblNewLabel_2;
 
 	/**
 	 * Create the panel.
@@ -25,9 +29,17 @@ public class CreditsPanel extends JPanel {
 	private void initPanel() {
 		setLayout(new BorderLayout(0, 0));
 		
-		panel = new JPanel();
-		add(panel);
-		panel.setLayout(new BorderLayout(0, 0));
+		centerPanel = new JPanel();
+		add(centerPanel, BorderLayout.CENTER);
+		
+		lblNewLabel = new JLabel("New label");
+		centerPanel.add(lblNewLabel);
+		
+		lblNewLabel_1 = new JLabel("New label");
+		centerPanel.add(lblNewLabel_1);
+		
+		lblNewLabel_2 = new JLabel("New label");
+		centerPanel.add(lblNewLabel_2);
 	}
 
 }
